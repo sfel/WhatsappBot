7cac5972cce96fad6888b4e5996edd6ad58d019b
sfel <zeec230@gmail.vom>
2020-03-24 21:23:23 +0200

    Added welcoming message before the link - with utf8 support :)

--- whatsappwebbot.py
+++ whatsappwebbot.py
@@ -12,17 +12,16 @@ from time import sleep
 DRIVER_PATH = r'.\chromedriver_win32\chromedriver.exe'
 PHONE_LIST_FILE=r'.\phones.txt'   # Assumes they are all valid
 COUNTRY_PREFIX = '972'
-MSG_TO_SEND = u'בלה בלה בלה'
-MSG_TO_SEND = MSG_TO_SEND.encode('utf-8')
 
 class WhatsupWebBot:
 	""" Automation class for whatsup """
 	group_name = 'LinkSendingBot'
 	link_to_send = 'TESTING123 www.google.com' # Dont forget to update it to whatever you want to send
+	welcome_msg = 'בלה בלה בלה'.encode('UTF-8').decode('UTF-8')
 
 	def __init__(self, phone_numbers):
 		self.__connect()
-		self.messages = [f'https://api.whatsapp.com/send?phone={number}&text=' for number in phone_numbers]
+		self.links_to_chats = [f'https://api.whatsapp.com/send?phone={number}&text=' for number in phone_numbers]
 
 	def __connect(self):
 		self.driver = webdriver.Chrome(executable_path=DRIVER_PATH)
@@ -39,13 +38,22 @@ class WhatsupWebBot:
 		user = self.driver.find_element_by_xpath(f'//span[@title = "{WhatsupWebBot.group_name}"]')
 		user.click()
 
-	def __send_message(self, msg):
+	def send_message_with_link(self, link):
+		send_message_js =  'var event = new InputEvent("input", {bubbles: true});'
+		send_message_js += 'var textbox = document.getElementsByClassName("_3u328")[1];'
+		send_message_js += f'textbox.textContent = "{WhatsupWebBot.welcome_msg}";'
+		send_message_js += 'textbox.dispatchEvent(event);'
+		send_message_js += 'document.querySelector("button._3M-N-").click()'
+		self.driver.execute_script(send_message_js)  # Sends the welcome message (encoded to utf-8)
+		self.__send_link(link)
+
+	def __send_link(self, link):
 		""" Send's a message """
 		msg_box = self.driver.find_elements_by_class_name('_3u328')[1]
-		msg_box.send_keys(msg)
+		msg_box.send_keys(link)
 		self.__click_send()
 
-	def __click_on_last_sent_message(self, msg):
+	def __click_on_last_sent_message(self):
 		""" Clicks on the last message that the bot sent """
 		self.driver.find_elements_by_class_name('message-out')[-1].click()
 		sleep(3)
@@ -55,16 +63,16 @@ class WhatsupWebBot:
 		return len(self.driver.find_elements_by_xpath("//*[contains(text(), 'Phone number shared via url is invalid.')]")) == 0
 
 	def run(self):
-		for msg in self.messages:
+		for link in self.links_to_chats:
 			self.__enter_spamming_group()
-			self.__send_message(msg)
-			self.__click_on_last_sent_message(msg)
+			self.__send_link(link)
+			self.__click_on_last_sent_message()
 			if self.__is_valid_number():
-				self.__send_message(WhatsupWebBot.link_to_send)
+				self.send_message_with_link(WhatsupWebBot.link_to_send)
 			else:
 				self.driver.find_element_by_class_name('_2eK7W').click()
-				print(f'The message - {msg} - contains bad number')
-a =None
+				print(f'The message - {link} - contains bad number')
+a = None
 def main():
 	global a
 	with open(PHONE_LIST_FILE) as phone_file:
